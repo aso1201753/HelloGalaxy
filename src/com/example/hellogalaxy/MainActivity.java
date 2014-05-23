802eafd418049c9d8a716a8867281a5425707b52
student <student@PC111S36.fgakusei.local>
2014-05-23 11:17:50 +0900

    じょうしせん　3Ｂ　末延　ハローギャラクシー

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.hellogalaxy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-
-public class MainActivity extends Activity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends Activity implements
+View.OnClickListener
+{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +17,35 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		btnOK.setOnClickListener(this);
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+switch(v.getId()){
+	case R.id.btnOK:
+		EditText etv = (EditText)findViewById(R.id.edtFirst);
+				String inputMsg = etv.getText().toString();
+		EditText etv2 = (EditText)findViewById(R.id.edtFamily);
+				String inputMsg2 = etv2.getText().toString();
+
+				TextView tv = (TextView)findViewById(R.id.txvMsg);
+				tv.setText( "あなたでしたか、" + inputMsg + inputMsg2 + "さん。");
+		}
+
 	}
 
 }
